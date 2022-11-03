2b6cc630cc3a6e643893e5c7a9cdba7a356a03ab
op06072 <op06072@gmail.com>
2022-11-03 09:21:36 +0900

    clean package.json

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import ejs from 'ejs';
 import path from 'path';
-import dotenv from 'dotenv';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { verifyToken } from './modules/jwt/index.js';
 import { schema, resolver } from './graphQL/schema.js';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 // dotenv.config({
 // 	path : process.env.NODE_ENV === "development" ? ".env.development" : ".env.production"
@@ -37,4 +36,4 @@ app.use(verifyToken);
 let server = app.listen(3000, function(){
     console.log("Express server has started on port 3000");
 });
-//var router = require('./router/main')(app, fs);
+// var router = require('./router/main')(app, fs);
