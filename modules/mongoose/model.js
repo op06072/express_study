6c0dfb80684fe6432229994fdaff1576dee4b394
op06072 <op06072@gmail.com>
2022-11-07 18:21:56 +0900

    dockerize

--- modules/mongoose/model.js
+++ modules/mongoose/model.js
@@ -4,7 +4,8 @@ import fs from "fs";
 const secret_path = '/run/secrets/root_'
 const USERNAME = fs.readFileSync(secret_path+'id', 'utf8').trimStart().trimEnd() || 'root';
 const PASSWORD = fs.readFileSync(secret_path+'pw', 'utf8').trimStart().trimEnd() || 'root';
-const URI = process.env.MONGO_URI || "localhost";
+console.log(USERNAME, PASSWORD);
+const URI = process.env.MONGO_URI || "127.0.0.1";
 const DB = process.env.MONGO_DB || "mongoose";
 
 const db = mongoose.connection;
@@ -13,6 +14,6 @@ db.once("open", function () {
     console.log("Connected to mongod server");
 });
 
-mongoose.connect(`mongodb://${USERNAME}:${PASSWORD}@${URI}/${DB}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://${USERNAME}:${PASSWORD}@${URI}:27017/${DB}?authSource=admin`, { useNewUrlParser: true });
 
 export default mongoose;
